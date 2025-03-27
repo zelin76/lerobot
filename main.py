f643e587463d775b360da42b160022a405db790d
robot2 <joke@you>
2025-03-27 20:07:51 +0800

    opt example

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
 import sys
 from PyQt6.QtWidgets import QApplication
-from ui.main_window import MainWindow
+
+from lerobot.scripts.train_plus import load_pretrained_model
 import os
 def main():
-    #model_path="outputs/train/test3/checkpoints/last"
-    #model_path=os.path.join( "outputs/train/test3/checkpoints/",  os.readlink(model_path))
-    #step,policy, optimizer, lr_scheduler=load_checkpoint(checkout_dir=model_path)
-    #step,policy, optimizer, lr_scheduler=load_pretrained_model(model_path=model_path, dataset_path="outputs/dataset/test3")
-    #step,policy, optimizer, lr_scheduler=load_pretrained_model(model_path="D:/github/lerobot/outputs/train/test3", dataset_path="D:/github/lerobot/outputs/dataset/test3")
-    
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    sys.exit(app.exec())
+    model_path="outputs/train/test3/checkpoints/last"
+    model_path=os.path.join( "outputs/train/test3/checkpoints/",  os.readlink(model_path))
+    step,policy, optimizer, lr_scheduler=load_pretrained_model(model_path=model_path, dataset_path="outputs/dataset/test3")
+    print("every thing ok")
 
 if __name__ == "__main__":
     main()
